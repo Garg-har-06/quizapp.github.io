61d1ca50c9b519171fd755c502a7425bbbbfb6c8
Garg-har-06 <gargharshita624@gmail.com>
2022-05-05 16:38:48 +0530

    quizapp

---
+++ js/question.js
@@ -0,0 +1,35 @@
+
+
+//array of objects
+const quiz=[
+    {
+        q:'Which module in Python supports regular expressions',
+        options:['re','regex','pyregex','None of the above'],
+        answer:0
+    },
+    
+    {
+        q:'To start Python from the command prompt, use the command ______',
+        options:['execute python','go python','python','run python'],
+        answer:2
+    },
+
+    {
+        q:'Given a function that does not return any value, what value is shown when executed at the shell?',
+        options:['int','bool','void','None'],
+        answer:3
+    },
+
+    {
+        q:'Which of these is not a core data type?',
+        options:['Lists','Dictionary','Tuples','Class'],
+        answer:3
+    },
+
+    {
+        q:'What is called when a function is defined inside a class?',
+        options:['Module','Class','Method','Another Function'],
+        answer:2
+    }
+    
+]
